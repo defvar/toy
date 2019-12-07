281bd5b117bc5d8cb18a1a6fb5d24e3562f75b09
defvar <def.daisuke@gmail.com>
2019-12-07 14:05:55 +0900

    add angular material

---
+++ apps/console/console-frontend/src/app/models/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', description: 'aaaaaaa' },
+  { id: 12, name: 'Narco', description: 'aaaaaaa' },
+  { id: 13, name: 'Bombasto', description: 'aaaaaaa' },
+  { id: 14, name: 'Celeritas', description: 'aaaaaaa' },
+  { id: 15, name: 'Magneta', description: 'aaaaaaa' },
+  { id: 16, name: 'RubberMan', description: 'aaaaaaa' },
+  { id: 17, name: 'Dynama', description: 'aaaaaaa' },
+  { id: 18, name: 'Dr IQ', description: 'aaaaaaa' },
+  { id: 19, name: 'Magma', description: 'aaaaaaa' },
+  { id: 20, name: 'Tornado', description: 'aaaaaaa' }
+];
