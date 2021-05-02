153ffde2bf8b32ca1b79f71c9978b817da7bf103
defvar <def.daisuke@gmail.com>
2021-05-03 00:04:11 +0900

    #21 supervisor retry api-server connection.

--- pkg/toy-supervisor/src/watcher.rs
+++ pkg/toy-supervisor/src/watcher.rs
@@ -1,11 +1,15 @@
 use crate::{Request, RunTaskResponse};
 use futures_util::stream::{self, TryStreamExt};
+use std::time::Duration;
 use toy_api::task::{AllocateOption, AllocateRequest, PendingEntity, WatchOption};
 use toy_api_client::client::TaskClient;
 use toy_api_client::ApiClient;
 use toy_core::error::ServiceError;
 use toy_core::graph::Graph;
 use toy_core::mpsc::Outgoing;
+
+const RETRY_MAX: u32 = 10;
+const RETRY_WAIT_SECS: u32 = 3;
 
 pub async fn watch<C>(
     name: String,
@@ -15,6 +19,7 @@ pub async fn watch<C>(
 where
     C: ApiClient + Clone,
 {
+    let mut retry_count = 0;
     loop {
         match c.task().watch(WatchOption::new()).await {
             Ok(st) => {
@@ -35,7 +40,12 @@ where
                     .await;
             }
             Err(e) => {
+                if retry_count >= RETRY_MAX {
+                    return Err(ServiceError::error(e));
+                }
                 tracing::error!(err = ?e, "an error occured; supervisor when watch task. rerun watcher.");
+                retry_count += 1;
+                std::thread::sleep(Duration::from_secs((RETRY_WAIT_SECS * retry_count) as u64));
             }
         }
     }
