688c23efc025c86bee230267b45515389916f107
daisuke <def.daisuke@gmail.com>
2021-08-13 21:59:25 +0900

    update influxdb version

--- docker/influxdb/influxdb-run.sh
+++ docker/influxdb/influxdb-run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-mkdir -p /tmp/influxdb-data.tmp
-sudo docker container rm -f  influxdb
+mkdir -p /var/influxdb-data
+sudo docker container rm -f influxdb
 sudo docker run -d \
 --network toy \
 --name influxdb \
 -p 8086:8086 \
---mount type=bind,source=/tmp/influxdb-data.tmp,destination=/root/.influxdbv2 \
-quay.io/influxdb/influxdb:v2.0.3
+--mount type=bind,source=/var/influxdb-data,destination=/var/lib/influxdb2 \
+quay.io/influxdb/influxdb:v2.0.7
