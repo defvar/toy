e202b773d7d4d5418436be0f8c4cbce627313152
daisuke <def.daisuke@gmail.com>
2021-11-06 21:57:21 +0900

    docker: improve fluent-bit.conf

--- docker/supervisor/run.sh
+++ docker/supervisor/run.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-if [ $# != 2 ]; then
-    echo "args: [env-file] [mount dir for secret]"
+if [ $# != 3 ]; then
+    echo "args: [env-file] [mount dir for secret] [name]"
     exit 1
 fi
 
 ENV_FILE="$1"
 SECRET="$2"
+NAME="$3"
 
 sudo docker run -d \
 --network toy \
 --log-driver=fluentd \
 --log-opt fluentd-address=localhost:24224 \
 --mount type=bind,source=/"$SECRET",destination=/.keys \
+--env TOY_SUPERVISOR_NAME="$NAME" \
 --env-file "$ENV_FILE" \
 toy/supervisor
