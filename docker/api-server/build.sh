ccb66d3481a2e5e0e115e499c1aa419ec90f8856
daisuke <def.daisuke@gmail.com>
2022-10-23 00:37:46 +0900

    docker: toy-api-d fix source path.

--- docker/api-server/build.sh
+++ docker/api-server/build.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$(cd $(dirname "$0");pwd)
 
-sudo docker build -t toy/console-backend -f "$SCRIPT_DIR"/Dockerfile "$SCRIPT_DIR"/../../
+sudo docker build -t toy/toyapi-d -f "$SCRIPT_DIR"/Dockerfile "$SCRIPT_DIR"/../../
