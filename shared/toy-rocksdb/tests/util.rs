37c51c365fd34a266da1fb74065b00692916a0c4
daisuke <def.daisuke@gmail.com>
2022-12-11 00:28:36 +0900

    toy-rocksdb: fix test (temp dir)

--- shared/toy-rocksdb/tests/util.rs
+++ shared/toy-rocksdb/tests/util.rs
@@ -1,7 +1,9 @@
+use tempdir::TempDir;
 use toy_rocksdb::Client;
 
 pub fn setup(cf: &str) -> Client {
-    let path = format!("/tmp/toy-rocksdb-test/{}", cf);
+    let tmp_dir = TempDir::new("toy-rocksdb-test").unwrap();
+    let path = tmp_dir.path().join(cf);
     let mut opt = rocksdb::Options::default();
     opt.create_if_missing(true);
     opt.create_missing_column_families(true);
