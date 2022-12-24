9b35b4a66ebda96adb55be6d0279af0818f45486
daisuke <def.daisuke@gmail.com>
2022-12-24 16:58:40 +0900

    toy-api-store-etcd: Change error messages for Display.

--- pkg/toy-api-store-etcd/src/error.rs
+++ pkg/toy-api-store-etcd/src/error.rs
@@ -6,28 +6,28 @@ use toy_api_server::store::error::StoreErrorCustom;
 
 #[derive(Debug, Error)]
 pub enum EtcdError {
-    #[error("invalid base64 string: {:?}", source)]
+    #[error("{source}")]
     InvalidBase64String {
         #[from]
         source: base64::DecodeError,
         backtrace: Backtrace,
     },
 
-    #[error("invalid utf-8 string: {:?}", source)]
+    #[error("{source}")]
     InvalidUTF8String {
         #[from]
         source: std::str::Utf8Error,
         backtrace: Backtrace,
     },
 
-    #[error("invalid version string: {:?}", source)]
+    #[error("invalid version string: {source}")]
     InvalidVersionString {
         #[from]
         source: ParseIntError,
         backtrace: Backtrace,
     },
 
-    #[error("error: {:?}", inner)]
+    #[error("error: {inner}")]
     Error { inner: String },
 }
 
