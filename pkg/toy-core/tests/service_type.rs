6d564005659122a7b1c4341cc0ddc6507833b48f
daisuke <def.daisuke@gmail.com>
2022-12-25 01:45:01 +0900

    toy-core: add test service_type ser/de

--- pkg/toy-core/tests/service_type.rs
+++ pkg/toy-core/tests/service_type.rs
@@ -7,3 +7,22 @@ fn st_from_str() {
         ServiceType::new("a.b", "c").unwrap()
     );
 }
+
+#[test]
+fn ser() {
+    assert_eq!(
+        std::str::from_utf8(
+            &toy_pack_json::pack(&ServiceType::from_full_name("a.b.c").unwrap()).unwrap()
+        )
+        .unwrap(),
+        "\"a.b.c\""
+    );
+}
+
+#[test]
+fn de() {
+    assert_eq!(
+        toy_pack_json::unpack::<ServiceType>("\"a.b.c\"".as_bytes()).unwrap(),
+        ServiceType::from_full_name("a.b.c").unwrap()
+    );
+}
