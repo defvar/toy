0037bd1a6be3fe1b094eab3c12ff6b8b301067db
daisuke <def.daisuke@gmail.com>
2022-12-29 19:39:20 +0900

    docker: update influxdb 2.5.1 -> 2.6.0

--- docker/influxdb/influxdb-run.sh
+++ docker/influxdb/influxdb-run.sh
@@ -6,6 +6,6 @@ sudo docker run -d \
 --network toy \
 --name influxdb \
 -p 8086:8086 \
-influxdb:2.5.1
+influxdb:2.6.0
 
 #--mount type=bind,source=/var/influxdb-data,destination=/var/lib/influxdb2 \
