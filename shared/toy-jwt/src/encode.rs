b207058a27ebfe343a644905baae995d67852a03
daisuke <def.daisuke@gmail.com>
2025-12-01 01:18:24 +0900

    tpy-jwt: add comment.

--- shared/toy-jwt/src/encode.rs
+++ shared/toy-jwt/src/encode.rs
@@ -2,6 +2,12 @@ use crate::algorithm::Algorithm;
 use crate::error::JWTError;
 use serde::Serialize;
 
+/// Create JWS from pem file.
+///
+/// If you are loading a RSA key from a .pem file.
+/// This errors if the key is not a valid RSA key.
+/// Encode the header and claims given and sign the payload using the algorithm from the header and the key.
+/// Algorithm given is RSA. The key needs to be in the PEM format.
 pub fn from_rsa_pem<T: Serialize>(
     claims: &T,
     alg: Algorithm,
